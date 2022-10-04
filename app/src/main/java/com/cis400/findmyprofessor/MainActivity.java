223cad176911c5073ba3c668b99486169e593246
emosaku <mosakueni@gmail.com>
2022-10-03 20:29:11 -0400

    Initial commit

---
+++ app/src/main/java/com/cis400/findmyprofessor/MainActivity.java
@@ -0,0 +1,43 @@
+ package com.cis400.findmyprofessor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //Initialize private TextView(type) variable for the register button
+    private TextView register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize Register button
+        register = (TextView) findViewById(R.id.textView3);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        //Get Id of the click location
+        switch(v.getId()){
+            //Posible Cases below
+            case R.id.textView3:
+                //Take us to register user layout
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+
+        }
+
+
+
+    }
+
+
+}
